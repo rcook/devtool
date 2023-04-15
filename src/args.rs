1f6ecbe5f1d684b5b0c21c101a584093fe10d1d2
Richard Cook <rcook@rcook.org>
2023-04-15 14:45:56 -0700

    Include commit hash in help output

--- src/args.rs
+++ src/args.rs
@@ -20,19 +20,21 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 use clap::{Parser, Subcommand};
-use default_env::default_env;
-use git_version::git_version;
 use path_absolutize::Absolutize;
 use std::path::PathBuf;
 
-const DEVTOOL_VERSION: &str = default_env!("DEVTOOL_VERSION", git_version!());
+const PACKAGE_NAME: &str = env!("CARGO_PKG_NAME");
+const PACKAGE_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+const PACKAGE_VERSION: &str = env!("CARGO_PKG_VERSION");
+const PACKAGE_HOME_PAGE: &str = env!("CARGO_PKG_HOMEPAGE");
+const PACKAGE_BUILD_VERSION: Option<&str> = option_env!("RUST_CI_BUILD_VERSION");
 
 #[derive(Parser, Debug)]
 #[command(
-    name = env!("CARGO_PKG_NAME"),
-    about = format!("{} {}", env!("CARGO_PKG_DESCRIPTION"), DEVTOOL_VERSION),
-    after_help = format!("{}\nhttps://github.com/rcook/devtool", env!["CARGO_PKG_HOMEPAGE"]),
-    version = DEVTOOL_VERSION
+    name = PACKAGE_NAME,
+    version = PACKAGE_VERSION,
+    about = format!("{} {}", PACKAGE_DESCRIPTION, PACKAGE_VERSION),
+    after_help = format!("{}{}", PACKAGE_HOME_PAGE, PACKAGE_BUILD_VERSION.map(|x| format!("\n\n{}", x)).unwrap_or(String::from("")))
 )]
 pub struct Args {
     #[arg(global = true, help = "Trace/debug mode", short = 't', long = "debug")]
